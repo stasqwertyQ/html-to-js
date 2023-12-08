90923826c6cd284efbef7bd187ff08eb083e3c6e
stasqwertyQ <125751548+stasqwertyQ@users.noreply.github.com>
2023-12-08 20:03:27 +0200

    home---js 08/12/23

---
+++ index.js
@@ -0,0 +1,57 @@
+
+// завдання 1
+
+
+
+const butJS = document.querySelector(".castomButton");
+const inpJS = document.querySelector("InputText");
+
+document.getElementById("castomButton").addEventListener("click", function () {
+
+const inputValue = document.getElementById("InputText").value;
+
+  document.getElementById("castomButton").innerText = inputValue;
+});
+
+    
+
+
+
+
+// завдання 2
+
+
+const img = document.querySelector(".Images");
+
+img.src = "https://img.freepik.com/premium-photo/pink-natural-landscape-at-sunset_839169-26301.jpg";
+
+console.log(img);
+
+
+
+// завдання 3
+
+
+var imageElement = document.getElementById('superImg');
+
+
+imageElement.alt = 'super---mega--alt';
+    
+
+
+
+const link = document.getElementById('linl--H');
+
+link.href = 'https://edu.goiteens.com/uk/learn/17196429/15266038/15266398/homework';
+
+
+
+// 4 завдання
+
+
+
+const castom = document.getElementById('list--JS');
+
+console.log(castom.textContent);
+
+castom.textContent = 'JavaScript';
